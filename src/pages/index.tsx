79201a428bd3f9de8df7abe8fad592530ba1db40
Chris Bailey <chris@ghostly.io>
2021-08-01 12:50:45 -0400

    :sparkles: use config file for titles and wallet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { ObjktCard } from '../components/ObjktCard';
 import { fetchGraphQL, galleryQuery } from '../utils/galleryFetch';
+import { config } from '../config';
 
 type Props = {
   result: Objkt[];
@@ -9,13 +10,15 @@ export default function Home(props: Props) {
   return (
     <>
       <header className="p-4 mb-8 bg-indigo-300">
-        <h1 className="ml-2 text-2xl font-bold tracking-widest">GALERIE</h1>
+        <h1 className="ml-2 text-2xl font-bold tracking-widest">
+          {config.title ? config.title : 'galerie'}
+        </h1>
       </header>
       <main className="w-full mx-2 mb-10 md:container md:mx-auto">
         <section>
           <header className="w-full p-2 bg-pink-200 rounded shadow mb-3 flex items-center">
             <h2 className="ml-2 text-4xl font-bold tracking-wider text-black uppercase">
-              HEN Gallery
+              Collection
             </h2>
           </header>
           <section className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3 xl:grid-cols-4">
@@ -63,10 +66,8 @@ type Amount = {
 };
 
 export async function getStaticProps() {
-  const henWallet = process.env.HEN_WALLET;
-
   const { errors, data } = await fetchGraphQL(galleryQuery, 'gallery', {
-    address: henWallet ?? null
+    address: config.address
   });
   if (errors) throw new Error(errors);
   const result: Objkt[] = data.hic_et_nunc_token_holder.sort(
